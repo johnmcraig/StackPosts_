fdea5b5396c4b7794e5722d74cd0470d3ff6506a
johnmcraig <jmcraig32884@gmail.com>
2019-04-11 13:37:09 -0400

    Added form to reply modal

--- PostsClient/src/components/add-reply-modal.vue
+++ PostsClient/src/components/add-reply-modal.vue
@@ -1,5 +1,20 @@
 <template>
+  <b-modal id="addReplyModal" ref="addReplyModal">
+    <b-form @submit.prevent="onSubmit" @reset.prevent="onCancel">
+      <b-form-group label="Your Reply" label-for="replyInput">
+        <b-form-textarea
+          id="replyInput"
+          v-model="form.body"
+          placeholder="Enter your reply here..."
+          :rows="6"
+          :max-rows="10">
+        </b-form-textarea>
+      </b-form-group>
+    </b-form>
 
+    <button class="btn btn-primary float-right ml-2" type="submit">Submit</button>
+    <button class="btn btn-secondary float-right" type="reset">Cancel</button>
+  </b-modal>
 </template>
 
 <script>
@@ -9,5 +24,4 @@ export default {
 </script>
 
 <style>
-
 </style>
