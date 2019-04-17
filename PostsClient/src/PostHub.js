1f390bab4cbc89fff3affd607d609820059109a8
johnmcraig <jmcraig32884@gmail.com>
2019-04-17 13:36:49 -0400

    Configured connection lose to hub

--- PostsClient/src/PostHub.js
+++ PostsClient/src/PostHub.js
@@ -3,10 +3,21 @@ import { HubConnectionBuilder, LogLevel } from '@aspnet/signalr'
 export default {
   install (Vue) {
     const connection = new HubConnectionBuilder()
-      .withUrl('https://localhost:5001/post-hub')
+      .withUrl(`${Vue.proptotype.$http.defaults.baseURL}/post-hub`)
       .connectionLogging(LogLevel.Information)
       .build()
 
-    connection.start()
+    let startPromise = null
+    function start () {
+      startPromise = connection.start().catch(err => {
+        console.error('Failed to connect to hub', err)
+        return new Promise((resolve, reject) =>
+          setTimeout(() => start().then(resolve).catch(reject), 5000))
+      })
+      return startPromise()
+    }
+    connection.onclose(() => start())
+
+    start()
   }
 }
