2d4b7eb52f9106d415c7e670fe38e68ac8c1bca2
johnmcraig <jmcraig32884@gmail.com>
2019-06-03 23:47:03 -0400

    Removed context layer and cleaned up comments

--- src/StackPosts_/PostsAPI/Controllers/v2/PostsController.cs
+++ src/StackPosts_/PostsAPI/Controllers/v2/PostsController.cs
@@ -13,18 +13,15 @@ namespace PostsAPI.Controllers
     [ApiController]
     public class DataController : ControllerBase
     {
-        private readonly PostsDbContext _dbContext;
         private readonly IPostRepository _repo;
 
         public DataController(PostsDbContext dbContext, IPostRepository repo)
         {
-            _dbContext = dbContext;
             _repo = repo;
         }
 
-        // GET api/data
         [HttpGet()]
-        public async Task<IActionResult> GetPosts()
+        public async Task<ActionResult> GetPosts()
         {
             try 
             {
@@ -38,35 +35,31 @@ namespace PostsAPI.Controllers
             
         }
 
-        // GET api/data/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetPost(Guid id)
+        public async Task<ActionResult> GetPost(Guid id)
         {
             try 
             {
                 var post = await _repo.GetPost(id);
                 return new JsonResult(post);
             }
-            catch
+            catch(Exception ex)
             {
-                return StatusCode(500);
+                return StatusCode(500, $"Eternal server error: {ex}");
             }
         }
 
-        // POST api/data
         [HttpPost("")]
         public Post AddPost([FromBody] Post post)
         {
-            _repo.AddPost(post);
+            _repo.Add(post);
 
             return post;
         }
 
-        // PUT api/data/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value) { }
 
-        // DELETE api/data/5
         [HttpDelete("{id}")]
         public void DeleteById(int id) { }
     }
