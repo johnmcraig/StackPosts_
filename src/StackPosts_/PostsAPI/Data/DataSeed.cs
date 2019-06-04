a9fdfad69f73e6785d83eeb2fc7a3ced6229dd64
johnmcraig <jmcraig32884@gmail.com>
2019-06-04 14:07:14 -0400

    Refactored Seed class and removed seed json file

--- src/StackPosts_/PostsAPI/Data/DataSeed.cs
+++ src/StackPosts_/PostsAPI/Data/DataSeed.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using PostsAPI.Models;
 
@@ -14,23 +16,59 @@ namespace PostsAPI.Data
             _dbContext = dbContext;
         }
 
-        public void Seed()
+        public async Task SeedData()
         {
+            
+                
+            // var postData = System.IO.File.ReadAllText("Data/PostsSeed.json");
+
+            // var posts = JsonConvert.DeserializeObject<List<Post>>(postData);
+            // var replies = JsonConvert.DeserializeObject<List<Reply>>(postData);
+            
+            // foreach (var post in posts)
+            // {
+            //     post.Replies = replies;
+            //     _dbContext.AddRange(post);
+            // }
+            
+            // _dbContext.SaveChanges();  
+            await _dbContext.Database.EnsureCreatedAsync();
+
             if (_dbContext.Posts.Any())
                 return;
-                
-            var postData = System.IO.File.ReadAllText("Data/PostsSeed.json");
 
-            var posts = JsonConvert.DeserializeObject<List<Post>>(postData);
-            var replies = JsonConvert.DeserializeObject<List<Reply>>(postData);
-            
-            foreach (var post in posts)
+            if(!_dbContext.Posts.Any())
             {
-                post.Replies = replies;
-                _dbContext.AddRange(post);
-            }
-            
-            _dbContext.SaveChanges();            
+                SeedPosts();
+                await _dbContext.SaveChangesAsync();
+            }        
         }
+
+        private void SeedPosts()
+        {
+            var posts = new List<Post>()
+            {
+                new Post
+                { 
+                    Id = Guid.NewGuid(),
+                    Title = "Welcome to the example Post",
+                    Body = "Welcome to this demonstration of making a Stack Overflow clone using ASP.Net Core 2.2 and Vue.js 2.6",
+                    Score = 0,
+                    Deleted = false,
+                    Replies = new List<Reply>{ new Reply { Body = "Awesome! Thanks."} } 
+                },
+                new Post
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "C# Basic Question",
+                    Body = "What is DI (Dependency Injection)?",
+                    Score = 1,
+                    Deleted = false,
+                    Replies = new List<Reply>{ new Reply { Body = "It is a way to initialize an object in one place and extend/use its properties throughout a class, method, and application."} } 
+                }
+            };
+
+            _dbContext.AddRange(posts);
+        } 
     }
 }
