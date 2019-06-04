6145078f1ab566ede8a97c7899123d64b788650e
johnmcraig <jmcraig32884@gmail.com>
2019-06-04 14:13:37 -0400

    Renamed model classes for mapping

--- src/StackPosts_/PostsAPI/Models/ReplyModel.cs
+++ src/StackPosts_/PostsAPI/Models/ReplyModel.cs
@@ -4,7 +4,6 @@ namespace PostsAPI.Models
 {
     public class Reply
     {
-        public Guid Id { get; set; }
         public Guid PostId { get; set; } // call post id to assocaite reply to same post
         public string Body { get; set; } //aka Body text of reply to post
         public int Score { get; set; }
