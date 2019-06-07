763d28ca3ca1796b8fcf6dee3c986fc9684de4cb
johnmcraig <jmcraig32884@gmail.com>
2019-06-07 00:19:44 -0400

    Added in memory db and config

--- src/StackPosts_/PostsAPI/Data/PostsDbContext.cs
+++ src/StackPosts_/PostsAPI/Data/PostsDbContext.cs
@@ -1,16 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PostsAPI.Data.Entities;
 
 namespace PostsAPI.Data
 {
     public class PostsDbContext : DbContext
     {
-        public PostsDbContext(DbContextOptions<PostsDbContext> options) : base(options)
+        private readonly IConfiguration _config;
+        public PostsDbContext(DbContextOptions<PostsDbContext> options, IConfiguration config) : base(options)
         {
-            Database.EnsureCreated();
+            _config = config;
+            // Database.EnsureCreated();
         }
 
         public DbSet<Post> Posts { get; set; }
         public DbSet<Reply> Replies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Employees");
+        }
     }
 }
