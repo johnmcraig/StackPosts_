43540048006e444c9b41b4809304e3cdb989517a
johnmcraig <jmcraig32884@gmail.com>
2020-02-24 18:22:27 -0500

    Added new migration based on entities

---
+++ src/StackPosts_/PostsAPI/Migrations/20200224215612_initialcreate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StackPosts_.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Body = table.Column<string>(nullable: false),
+                    Score = table.Column<int>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    DatePosted = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Replies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    PostId = table.Column<Guid>(nullable: false),
+                    Body = table.Column<string>(nullable: true),
+                    Score = table.Column<int>(nullable: false),
+                    DateReplied = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Replies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Replies");
+        }
+    }
+}
