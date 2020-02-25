6040be3d9aca75c23dd342f664bdcb7495456a59
johnmcraig <jmcraig32884@gmail.com>
2020-02-25 00:57:54 -0500

    Created disposed build for migration and seeding of db

--- src/StackPosts_/PostsAPI/Program.cs
+++ src/StackPosts_/PostsAPI/Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PostsAPI.Data;
 
 namespace PostsAPI
 {
@@ -14,7 +17,26 @@ namespace PostsAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var dbContext = services.GetRequiredService<PostsDbContext>();
+                    dbContext.Database.Migrate();
+                    Seed.SeedData(dbContext);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError($"Could not create or migrate database, see full error: {ex}");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
