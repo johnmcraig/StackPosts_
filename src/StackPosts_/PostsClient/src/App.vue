5d32aaa505d26a905b284f00447021e4aeff7dc0
johnmcraig <jmcraig32884@gmail.com>
2020-02-25 00:57:33 -0500

    changed back to anchor tag

--- src/StackPosts_/PostsClient/src/App.vue
+++ src/StackPosts_/PostsClient/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow">
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow">
       <a class="navbar-brand" href="/">StackPosts_</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
       <div class="collapse navbar-collapse" id="main-navbar">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <router-link class="nav-link" :to="Home">Home <span class="sr-only">(current)</span></router-link>
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
