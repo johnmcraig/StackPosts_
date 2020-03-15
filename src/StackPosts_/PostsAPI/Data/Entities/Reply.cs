134f83ef357f45101dcfb6e7e0583fd58dc1fc42
johnmcraig <jmcraig32884@gmail.com>
2020-03-14 23:03:33 -0400

    spelling correction in comment

--- src/StackPosts_/PostsAPI/Data/Entities/Reply.cs
+++ src/StackPosts_/PostsAPI/Data/Entities/Reply.cs
@@ -5,7 +5,7 @@ namespace PostsAPI.Data.Entities
     public class Reply
     {
         public Guid Id { get; set; }
-        public Guid PostId { get; set; } // call post id to assocaite reply to same post
+        public Guid PostId { get; set; } // call post id to associate reply to same post
         public string Body { get; set; } //aka Body text of reply to post
         public int Score { get; set; }
         public DateTime DateReplied { get; set; }
