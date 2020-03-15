190e033b51034d661d91dff15e9886095b6c746d
johnmcraig <jmcraig32884@gmail.com>
2020-03-14 22:54:32 -0400

    spelling correction in comment

--- src/StackPosts_/PostsAPI/Models/ReplyModel.cs
+++ src/StackPosts_/PostsAPI/Models/ReplyModel.cs
@@ -4,7 +4,7 @@ namespace PostsAPI.Models
 {
     public class ReplyModel
     {   
-        public Guid PostId { get; set; } // call post id to assocaite reply to same post
+        public Guid PostId { get; set; } // call post id to associate reply to same post
         public string Body { get; set; } //aka Body text of reply to post
         public int Score { get; set; }
         public bool Deleted { get; set; }
