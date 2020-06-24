eaac991bcf178e5df37e026142ff3e355e730811
johnmcraig <jmcraig32884@gmail.com>
2020-06-23 21:20:41 -0400

    Refactored Guid to int for easier DB computation

--- src/StackPosts_/PostsAPI/Models/ReplyModel.cs
+++ src/StackPosts_/PostsAPI/Models/ReplyModel.cs
@@ -4,11 +4,10 @@ namespace PostsAPI.Models
 {
     public class ReplyModel
     {   
-        public Guid PostId { get; set; } // call post id to associate reply to same post
+        public int PostId { get; set; } // call post id to associate reply to same post
         public string Body { get; set; } //aka Body text of reply to post
         public int Score { get; set; }
         public bool Deleted { get; set; }
-        public DateTime DateReplied { get; set; }
-        
+        public DateTime DateReplied { get; set; }  
     }
 }
