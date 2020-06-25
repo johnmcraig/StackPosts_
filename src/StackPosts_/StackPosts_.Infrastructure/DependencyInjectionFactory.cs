2d744522f74b686d391e79024164b72374e92de3
johnmcraig <jmcraig32884@gmail.com>
2020-06-24 21:46:08 -0400

    Created DI factory class to be injected in Startup of Api project

---
+++ src/StackPosts_/StackPosts_.Infrastructure/DependencyInjectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StackPosts_.Core.Interfaces;
+using StackPosts_.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackPosts_.Infrastructure
+{
+    public static class DependencyInjectionFactory
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IPostRepository, PostRepository>();
+            services.AddDbContext<StoreContext>();
+
+            return services;
+        }
+    }
+}
