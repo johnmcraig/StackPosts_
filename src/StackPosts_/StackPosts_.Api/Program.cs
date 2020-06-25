41703e16024d64795b061d2c7893d361bc634b85
johnmcraig <jmcraig32884@gmail.com>
2020-06-24 21:18:40 -0400

    Updated using statements and commented out disposing scope for now

--- src/StackPosts_/StackPosts_.Api/Program.cs
+++ src/StackPosts_/StackPosts_.Api/Program.cs
@@ -8,39 +8,44 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using PostsAPI.Data;
+using StackPosts_.Api.Data;
 
-namespace PostsAPI
+namespace StackPosts_.Api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
+            var host = CreateHostBuilder(args).Build();
             
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
+            //using (var scope = host.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+            //    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
 
-                try
-                {
-                    var dbContext = services.GetRequiredService<PostsDbContext>();
-                    dbContext.Database.Migrate();
-                    Seed.SeedData(dbContext);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError($"Could not create or migrate database, see full error: {ex}");
-                }
-            }
+            //    try
+            //    {
+            //        var dbContext = services.GetRequiredService<PostsDbContext>();
+            //        //dbContext.Database.Migrate();
+            //        // Seed.SeedData(dbContext);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var logger = loggerFactory.CreateLogger<Program>();
+            //        logger.LogError($"Could not create or migrate database, see full error: {ex}");
+            //    }
+            //}
 
             host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
