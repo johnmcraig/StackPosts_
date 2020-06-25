5b87609f0dccb172ed3c2734fce00ce074920fe9
johnmcraig <jmcraig32884@gmail.com>
2020-06-24 23:57:12 -0400

    Created a fallback controller to read static files and serve them

---
+++ src/StackPosts_/StackPosts_.Api/Controllers/FallbackController.cs
@@ -0,0 +1,10 @@
+namespace StackPosts_.Api.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+        }
+    }
+}
