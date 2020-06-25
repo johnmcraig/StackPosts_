7c101f1fabe7efaa4261ee3c1bae09fe881bea54
johnmcraig <jmcraig32884@gmail.com>
2020-06-24 23:53:04 -0400

    Line space

--- src/StackPosts_/StackPosts_.Core/Interfaces/IGenericRepository.cs
+++ src/StackPosts_/StackPosts_.Core/Interfaces/IGenericRepository.cs
@@ -4,7 +4,7 @@ namespace StackPosts_.Core.Interfaces
 {
     public interface IGenericRepository
     {
-         void Add<T>(T entity) where T: class;
+        void Add<T>(T entity) where T: class;
         void Delete<T>(T entity) where T: class;
         Task<bool> SaveChangesAsync();
     }
