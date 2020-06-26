f24ce2e935db0e5a74da94a5b3dc65418a50d89a
johnmcraig <jmcraig32884@gmail.com>
2020-06-25 23:47:11 -0400

    Created IUnitOfWork interface

---
+++ src/StackPosts_/StackPosts_.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using StackPosts_.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackPosts_.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+        Task<int> Complete();
+    }
+}
