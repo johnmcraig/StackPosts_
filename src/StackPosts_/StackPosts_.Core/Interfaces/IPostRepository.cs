c2da4181bd84793f9e46cfcaf17642f3369bdc81
johnmcraig <jmcraig32884@gmail.com>
2020-06-25 23:48:21 -0400

    Refactored interface to reflect naming conventions and IGenereicRepo using BaseEntity

--- src/StackPosts_/StackPosts_.Core/Interfaces/IPostRepository.cs
+++ src/StackPosts_/StackPosts_.Core/Interfaces/IPostRepository.cs
@@ -5,14 +5,11 @@ using StackPosts_.Core.Entities;
 
 namespace StackPosts_.Core.Interfaces
 {
-    public interface IPostRepository : IGenericRepository
-    {
-        void AddReply(Reply reply);
-        
-        Task<Post[]> GetPosts();
-        Task<Post> GetPost(int id);
+    public interface IPostRepository : IGenericRepository<Post>
+    {   
+        Task<IReadOnlyList<Post>> GetPostsAsync();
+        Task<Post> GetPostByIdAsync(int id);
         Task<Post[]> GetPostByTitle(string title, bool includeReplies = false);
         Task<Post[]> GetByDatePosted(DateTime date);
-        
     }
 }
