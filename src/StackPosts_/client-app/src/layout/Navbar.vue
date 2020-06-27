2843db946b81a1d8b42aa25efee0d52c05f68ccc
johnmcraig <jmcraig32884@gmail.com>
2020-06-26 23:41:26 -0400

    Created Navbar

---
+++ src/StackPosts_/client-app/src/layout/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow">
+      <a class="navbar-brand" href="/">StackPosts_</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="main-navbar">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/login">Login </router-link>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+</template>
