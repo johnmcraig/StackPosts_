5b66bd080cd5fc7a631c2faea2442cc661b37559
johnmcraig <jmcraig32884@gmail.com>
2020-06-26 23:41:11 -0400

    Added path to login and set mode to history

--- src/StackPosts_/client-app/src/router.js
+++ src/StackPosts_/client-app/src/router.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/views/Home.vue'
 import PostPage from '@/views/Post.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,6 +18,11 @@ export default new Router({
       path: '/post/:id',
       name: 'Post',
       component: PostPage
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
