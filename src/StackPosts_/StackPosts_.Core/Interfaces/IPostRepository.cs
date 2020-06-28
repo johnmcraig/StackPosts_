d4ab5867a4e7a6ec7091d258127623fe4eab29e2
johnmcraig <jmcraig32884@gmail.com>
2020-06-28 16:05:45 -0400

    Changed from Array to IEnumerable for easier testing

--- src/StackPosts_/StackPosts_.Core/Interfaces/IPostRepository.cs
+++ src/StackPosts_/StackPosts_.Core/Interfaces/IPostRepository.cs
@@ -7,7 +7,7 @@ namespace StackPosts_.Core.Interfaces
 {
     public interface IPostRepository : IGenericRepository<Post>
     {   
-        Task<Post[]> GetPostsAsync();
+        Task<IEnumerable<Post>> GetPostsAsync();
         Task<Post> GetPostByIdAsync(int id);
         Task<Post[]> GetPostByTitle(string title, bool includeReplies = false);
         Task<Post[]> GetByDatePosted(DateTime date);
