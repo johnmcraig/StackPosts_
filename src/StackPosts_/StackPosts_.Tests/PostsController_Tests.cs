afe73491d77d7af37a741a0f5a18cac1ea5553e4
johnmcraig <jmcraig32884@gmail.com>
2020-06-28 16:04:09 -0400

    Commented out most of test class as it is unused

--- src/StackPosts_/StackPosts_.Tests/PostsController_Tests.cs
+++ src/StackPosts_/StackPosts_.Tests/PostsController_Tests.cs
@@ -1,64 +1,62 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using StackPosts_.Api.Controllers.v2;
+using StackPosts_.Api.Controllers;
+using StackPosts_.Api.Data;
 using StackPosts_.Api.Data.Entities;
-using StackPosts_.Core.Interfaces;
 using StackPosts_.Infrastructure.Data;
 using System;
-using System.Collections.Generic;
-using System.Text;
+
 using Xunit;
 
 namespace StackPosts_.Tests
 {
     public class PostsController_Tests
     {
-        [Fact]
-        public async void GetPost()
-        {
-            #region Arrange
+        //[Fact]
+        //public async void GetPost()
+        //{
+        //    #region Arrange
 
-            var options = new DbContextOptionsBuilder<StoreContext>()
-                .UseInMemoryDatabase(databaseName: "StackPosts")
-                .Options;
+        //    var options = new DbContextOptionsBuilder<PostsDbContext>()
+        //        .UseInMemoryDatabase(databaseName: "StackPosts")
+        //        .Options;
 
-            using(var context = new StoreContext(options))
-            {
-                context.Add(new Post()
-                {
-                    Id = 1,
-                    Title = $"Test Post",
-                    Body = $"Test Content for Post",
-                    Score = 1,
-                    Deleted = false,
-                    DatePosted = DateTime.UtcNow,
-                });
+        //    using (var context = new PostsDbContext(options))
+        //    {
+        //        context.Add(new Post()
+        //        {
+        //            Id = 1,
+        //            Title = $"Test Post",
+        //            Body = $"Test Content for Post",
+        //            Score = 1,
+        //            Deleted = false,
+        //            DatePosted = DateTime.UtcNow,
+        //        });
 
-                context.SaveChanges(); 
-            }
+        //        context.SaveChanges();
+        //    }
 
-            Post post_existing = null;
-            Post post_notExisting = null;
+        //    Post post_existing = null;
+        //    Post post_notExisting = null;
 
-            #endregion
+        //    #endregion
 
-            #region Act
+        //    #region Act
 
-            using (var context = new StoreContext(options))
-            {
-                var controller = new PostsController(context);
-                post_existing = (await controller.GetPost(1)).Value;
-                post_notExisting = (await controller.GetPost(2)).Value;
-            }
+        //    using (var context = new PostsDbContext(options))
+        //    {
+        //        var controller = new PostsController(options);
+        //        post_existing = controller.GetPost(1).Value;
+        //        post_notExisting = controller.GetPost(2).Value;
+        //    }
 
-            #endregion
+        //    #endregion
 
-            #region Assert
+        //    #region Assert
 
-            Assert.True(post_existing != null && post_notExisting != null);
+        //    Assert.True(post_existing != null && post_notExisting != null);
 
-            #endregion
-
-        }
+        //    #endregion
+        //}
     }
 }
