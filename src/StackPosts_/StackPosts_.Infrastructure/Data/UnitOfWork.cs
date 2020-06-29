37f4bb321f2d16323778a75f7513a9b7a68df5ff
johnmcraig <jmcraig32884@gmail.com>
2020-06-28 21:54:18 -0400

    Line space fix

--- src/StackPosts_/StackPosts_.Infrastructure/Data/UnitOfWork.cs
+++ src/StackPosts_/StackPosts_.Infrastructure/Data/UnitOfWork.cs
@@ -28,7 +28,7 @@ namespace StackPosts_.Infrastructure.Data
 
     public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
     {
-        if(_repositories == null) _repositories = new Hashtable();
+        if (_repositories == null) _repositories = new Hashtable();
 
         var type = typeof(TEntity).Name;
         
