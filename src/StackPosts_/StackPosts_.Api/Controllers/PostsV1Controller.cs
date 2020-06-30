8c7c48f68206dc84bf79554aa662526d3529a5b7
johnmcraig <jmcraig32884@gmail.com>
2020-06-29 23:19:00 -0400

    Renamed and move v1 controller

---
+++ src/StackPosts_/StackPosts_.Api/Controllers/PostsV1Controller.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using StackPosts_.Api.Hubs;
+using StackPosts_.Core.Interfaces;
+using StackPosts_.Core.Entities;
+
+namespace StackPosts_.Api.Controllers
+{
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class PostsV1Controller : ControllerBase 
+    {
+        private readonly IHubContext<PostHub, IPostHub> _hubContext;
+        private readonly IPostRepository _postRepo;
+
+        public PostsV1Controller(IHubContext<PostHub, IPostHub> postHub, IPostRepository postRepo)
+        {
+            _hubContext = postHub;
+            _postRepo = postRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Post>> GetPosts()
+        { 
+            var posts = await _postRepo.GetPostsAsync();
+            return posts;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> GetPost(int id)
+        {
+            var post = await _postRepo.GetPostByIdAsync(id);
+            
+            if (post == null) return NotFound();
+
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Post>> AddPost([FromBody] Post post)
+        {
+            if(!ModelState.IsValid) return NotFound();
+
+            _postRepo.Add(post);
+
+            // post.Id = Guid.NewGuid();
+            // post.Deleted = false;
+            // post.Replies = new List<Reply>();
+            // posts.Add(post);
+
+            await _postRepo.SaveChangesAsync();
+
+            return CreatedAtRoute(nameof(GetPost), new { id = post.Id }, post);
+        }
+
+        [HttpPost("{id}/reply")]
+        public async Task<ActionResult> AddReplyAsync(int id, [FromBody] Core.Entities.Reply reply)
+        {
+            var post = await _postRepo.GetPostByIdAsync(id);
+
+            if (post == null) return NotFound();
+
+            reply.Id = id;
+            reply.PostId = id;
+            reply.Deleted = false;
+            post.Replies.Add(reply);
+
+            await _hubContext.Clients.Group(id.ToString()).ReplyAdded(reply);
+            
+            await _hubContext.Clients.All.ReplyCountChange(post.Id, post.Replies.Count);
+
+            return new JsonResult(reply);
+        }
+
+        [HttpPatch("{id}/upvote")]
+        public async Task<ActionResult> UpvotePostAsync(int id)
+        {
+            var post = await _postRepo.GetPostByIdAsync(id);
+
+            if (post == null) return NotFound();
+
+            // Warning, this is not thread-safe. Use interlocked methods.
+            post.Score++;
+
+            await _hubContext.Clients.All.PostScoreChange(post.Id, post.Score);
+
+            return new JsonResult(post);
+        }
+
+        [HttpPatch("{id}/downvote")]
+        public async Task<ActionResult> DownvotePostAsync(int id)
+        {
+            var post = await _postRepo.GetPostByIdAsync(id);
+
+            if (post == null) return NotFound();
+
+            post.Score--;
+
+            await _hubContext.Clients.All.PostScoreChange(post.Id, post.Score);
+
+            return new JsonResult(post);
+        }
+    }
+}
