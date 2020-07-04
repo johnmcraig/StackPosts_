96be2a5707a42dadca57273b0bfc72c545c027c1
johnmcraig <jmcraig32884@gmail.com>
2020-07-03 23:28:34 -0400

    Removed comment, added spacing for easier reading

--- src/StackPosts_/StackPosts_.Core/Entities/Post.cs
+++ src/StackPosts_/StackPosts_.Core/Entities/Post.cs
@@ -8,11 +8,16 @@ namespace StackPosts_.Core.Entities
     {   
         [Required]
         public string Title { get; set; }
+
         [Required]
         public string Body { get; set; }
-        public int Score { get; set; } // upvote & downvote tracking 
+
+        public int Score { get; set; }
+
         public bool Deleted { get; set; }
+
         public DateTime DatePosted { get; set; }
+
         public List<Reply> Replies;
     }
 }
