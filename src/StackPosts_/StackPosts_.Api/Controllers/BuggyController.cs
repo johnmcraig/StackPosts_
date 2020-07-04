d7c53cf11093fa6c5d250216f1dc186f9261367b
johnmcraig <jmcraig32884@gmail.com>
2020-07-03 22:29:43 -0400

    Created controller for specific errors

---
+++ src/StackPosts_/StackPosts_.Api/Controllers/BuggyController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackPosts_.Api.Errors;
+using StackPosts_.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackPosts_.Api.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _dbContext;
+
+        public BuggyController(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _dbContext.Posts.Find(42);
+
+            if (thing == null) return NotFound(new ApiResponse(404));
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _dbContext.Posts.Find(42);
+
+            if (thing == null) return NotFound();
+
+            var thingToRetun = thing.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequestById(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
