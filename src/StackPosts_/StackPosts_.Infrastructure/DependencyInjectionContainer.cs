51367ebc32e970222e0753e355f24b3d6fbab732
johnmcraig <jmcraig32884@gmail.com>
2020-07-04 21:04:59 -0400

    Renamed DI factory to DI container

--- src/StackPosts_/StackPosts_.Infrastructure/DependencyInjectionContainer.cs
+++ src/StackPosts_/StackPosts_.Infrastructure/DependencyInjectionContainer.cs
@@ -4,11 +4,14 @@ using StackPosts_.Infrastructure.Data;
 
 namespace StackPosts_.Infrastructure
 {
-    public static class DependencyInjectionFactory
+    public static class DependencyInjectionContainer
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddTransient<IPostRepository, PostRepository>();
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
             services.AddDbContext<StoreContext>();
 
             return services;
