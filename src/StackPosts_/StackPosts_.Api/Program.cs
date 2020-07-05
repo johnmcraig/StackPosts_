ff38570435694aea072b1eb12fcb626c5bb7f760
johnmcraig <jmcraig32884@gmail.com>
2020-07-04 20:59:34 -0400

    Refactored commented disposing block to create and migrate up startup

--- src/StackPosts_/StackPosts_.Api/Program.cs
+++ src/StackPosts_/StackPosts_.Api/Program.cs
@@ -1,42 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using StackPosts_.Api.Data;
+using StackPosts_.Infrastructure.Data;
+using StackPosts_.Infrastructure.Data.SeedData;
 
 namespace StackPosts_.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static  async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            
-            //using (var scope = host.Services.CreateScope())
-            //{
-            //    var services = scope.ServiceProvider;
-            //    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
 
-            //    try
-            //    {
-            //        var dbContext = services.GetRequiredService<PostsDbContext>();
-            //        //dbContext.Database.Migrate();
-            //        // Seed.SeedData(dbContext);
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        var logger = loggerFactory.CreateLogger<Program>();
-            //        logger.LogError($"Could not create or migrate database, see full error: {ex}");
-            //    }
-            //}
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var dbContext = services.GetRequiredService<StoreContext>();
+                    await dbContext.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(dbContext, loggerFactory);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError($"Could not create or migrate database, see full error: {ex}");
+                }
+            }
 
             host.Run();
         }
