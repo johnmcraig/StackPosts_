ce7e9d456b6935c29d323f76e546a59c7e983393
johnmcraig <jmcraig32884@gmail.com>
2020-07-04 21:12:18 -0400

    Commented out unused entities and context class to soon be deleted

--- src/StackPosts_/StackPosts_.Api/Data/Entities/Post.cs
+++ src/StackPosts_/StackPosts_.Api/Data/Entities/Post.cs
@@ -4,17 +4,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace StackPosts_.Api.Data.Entities
 {
-    public class Post
-    {   
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Body { get; set; }
-        public int Score { get; set; } // upvote & downvote tracking 
-        public bool Deleted { get; set; }
-        public DateTime DatePosted { get; set; }
-        public List<Reply> Replies;
-    }
+    //public class Post
+    //{   
+    //    [Key]
+    //    public int Id { get; set; }
+    //    [Required]
+    //    public string Title { get; set; }
+    //    [Required]
+    //    public string Body { get; set; }
+    //    public int Score { get; set; } // upvote & downvote tracking 
+    //    public bool Deleted { get; set; }
+    //    public DateTime DatePosted { get; set; }
+    //    public List<Reply> Replies;
+    //}
 }
--- src/StackPosts_/StackPosts_.Api/Data/PostsDbContext.cs
+++ src/StackPosts_/StackPosts_.Api/Data/PostsDbContext.cs
@@ -4,27 +4,27 @@ using StackPosts_.Api.Data.Entities;
 
 namespace StackPosts_.Api.Data
 {
-    public class PostsDbContext : DbContext
-    {
-        //private readonly IConfiguration _config;
-        public PostsDbContext(DbContextOptions<PostsDbContext> options) : base(options)
-        {
-            //, IConfiguration config
-            //_config = config;
-            // Database.EnsureCreated();
-        }
+    //public class PostsDbContext : DbContext
+    //{
+    //    //private readonly IConfiguration _config;
+    //    public PostsDbContext(DbContextOptions<PostsDbContext> options) : base(options)
+    //    {
+    //        //, IConfiguration config
+    //        //_config = config;
+    //        // Database.EnsureCreated();
+    //    }
 
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<Reply> Replies { get; set; }
+    //    public DbSet<Post> Posts { get; set; }
+    //    public DbSet<Reply> Replies { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
+    //    protected override void OnModelCreating(ModelBuilder builder)
+    //    {
+    //        base.OnModelCreating(builder);
+    //    }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //optionsBuilder.UseSqlServer(_config.GetConnectionString("sqlConString"));
-        }
-    }
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        //optionsBuilder.UseSqlServer(_config.GetConnectionString("sqlConString"));
+    //    }
+    //}
 }
