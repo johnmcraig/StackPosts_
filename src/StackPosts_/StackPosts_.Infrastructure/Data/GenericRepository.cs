716a497df7cc813616061e6a6b3e01371824ff3b
johnmcraig <jmcraig32884@gmail.com>
2020-07-04 21:05:46 -0400

    Implemented interface methods

--- src/StackPosts_/StackPosts_.Infrastructure/Data/GenericRepository.cs
+++ src/StackPosts_/StackPosts_.Infrastructure/Data/GenericRepository.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using StackPosts_.Core.Entities;
 using StackPosts_.Core.Interfaces;
 
@@ -6,19 +8,36 @@ namespace StackPosts_.Infrastructure.Data
 {
   public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
   {
-    public void Add(T entity)
-    {
-      throw new System.NotImplementedException();
-    }
+        private readonly StoreContext _dbContext;
 
-    public void Delete(T entity)
-    {
-      throw new System.NotImplementedException();
-    }
+        public GenericRepository(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-    public Task<bool> SaveChangesAsync()
-    {
-      throw new System.NotImplementedException();
-    }
+        public void Add(T entity)
+        {
+            _dbContext.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Remove(entity);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _dbContext.SaveChangesAsync()) > 0;
+        }
   }
 }
