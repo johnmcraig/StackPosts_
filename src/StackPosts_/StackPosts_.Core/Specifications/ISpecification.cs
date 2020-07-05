2109a583df3e51ae60a98a2ae5c3f2f3381eed81
johnmcraig <jmcraig32884@gmail.com>
2020-07-04 21:04:32 -0400

    Created interface for spec pattern and implemntation class

---
+++ src/StackPosts_/StackPosts_.Core/Specifications/ISpecification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace StackPosts_.Core.Specifications
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
