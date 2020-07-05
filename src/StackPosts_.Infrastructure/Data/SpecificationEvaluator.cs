f7c5ff1a21022483d215627e0daff746f69e8839
johnmcraig <jmcraig32884@gmail.com>
2020-07-05 00:58:11 -0400

    Created spec evaluation class for queries

---
+++ src/StackPosts_.Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using StackPosts_.Core.Entities;
+using StackPosts_.Core.Specifications;
+
+namespace StackPosts_.Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+
+            if (spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
