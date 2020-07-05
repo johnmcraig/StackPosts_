ed10b7272fdb5d64cf161b6680f4c91986df32cb
johnmcraig <jmcraig32884@gmail.com>
2020-07-04 21:02:05 -0400

    Added reply and post dtos

---
+++ src/StackPosts_/StackPosts_.Api/Dtos/ReplyToReturnDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackPosts_.Api.Dtos
+{
+    public class ReplyToReturnDto
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public string Body { get; set; }
+
+        public int Score { get; set; }
+
+        public DateTime DateReplied { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
