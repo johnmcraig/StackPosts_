37afc663d5d2d1146818022c4160f22c6013824f
johnmcraig <jmcraig32884@gmail.com>
2020-07-09 00:17:22 -0400

    Created store with Vuex, formated files with eslint, updated imports

--- src/client-app/src/main.js
+++ src/client-app/src/main.js
@@ -1,24 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import PostHub from './post-hub'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import PostHub from './post-hub';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // Setup axios
-axios.defaults.baseURL = 'https://localhost:5001/api'
-Vue.prototype.$http = axios
+axios.defaults.baseURL = 'https://localhost:5001/api';
+Vue.prototype.$http = axios;
 
 // Install Vue extensions
-Vue.use(BootstrapVue)
-Vue.use(PostHub)
+Vue.use(BootstrapVue);
+Vue.use(PostHub);
 
 new Vue({
+  store,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
---
+++ src/client-app/src/store/store.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Axios from 'axios';
+
+Vue.use(vuex);
+
+const baseUrl = 'api';
+
+const postsUrl = `${baseUrl}/posts`;
+
+export default Vuex.Store({
+  strict: true,
+  state: {
+    posts: [],
+    replies: [],
+  },
+  mutations: {
+    setPosts(state, posts) {
+      state.posts = posts;
+    },
+  },
+  actions: {
+    async getPosts(context) {
+      context.commit('setPosts', await Axios.get(postsUrl));
+    },
+  },
+});
