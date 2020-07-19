1d5831fbeb04e4a28fa9c3d99ebc48ac1fc5ce76
johnmcraig <jmcraig32884@gmail.com>
2020-07-19 16:41:38 -0400

    created interface repository for replies entity

---
+++ src/StackPosts_.Core/Interfaces/IReplyRepository.cs
@@ -0,0 +1,9 @@
+using StackPosts_.Core.Entities;
+
+namespace StackPosts_.Core.Interfaces
+{
+    public interface IReplyRepository : IGenericRepository<Reply>
+    {
+        /* A empty bridge */
+    }
+}
---
+++ src/StackPosts_.Infrastructure/Data/ReplyRepository.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using StackPosts_.Core.Entities;
+using StackPosts_.Core.Interfaces;
+
+namespace StackPosts_.Infrastructure.Data
+{
+  public class ReplyRepository : IReplyRepository
+  {
+       private readonly StoreContext _dbContext;
+        private readonly ILogger<ReplyRepository> _logger;
+
+        public ReplyRepository(StoreContext dbContext, ILogger<ReplyRepository> logger)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+    public void Add(Reply entity)
+    {
+        _logger.LogInformation($"Inserting entity");
+        _dbContext.Add(entity);
+    }
+
+    public void Delete(Reply entity)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Task<bool> SaveChangesAsync()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
