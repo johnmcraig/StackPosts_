762bc9ab64d8a2157d3eefa0d0a95f1a8cff643e
johnmcraig <jmcraig32884@gmail.com>
2020-07-21 21:29:10 -0400

    created data service for posts using es6 classes

---
+++ src/client-app/src/services/PostsDataService.js
@@ -0,0 +1,33 @@
+import http from "../http-common";
+
+class PostsDataService {
+  getAll() {
+    return http.get("/posts");
+  }
+
+  get(id) {
+    return http.get(`/posts/${id}`);
+  }
+
+  create(data) {
+    return http.post("/posts", data);
+  }
+
+  update(id, data) {
+    return http.put(`/posts/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/posts/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`/posts`);
+  }
+
+  findByTitle(title) {
+    return http.get(`/posts?title=${title}`);
+  }
+}
+
+export default new PostsDataService();
