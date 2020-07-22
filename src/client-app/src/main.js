796685c620564d265efcccd407169f49b3fb738e
johnmcraig <jmcraig32884@gmail.com>
2020-07-21 21:45:05 -0400

    commented out post-hub usage

--- src/client-app/src/main.js
+++ src/client-app/src/main.js
@@ -1,23 +1,23 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import axios from 'axios';
+import axios from "axios";
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import PostHub from './post-hub';
+//import PostHub from './post-hub';
 import store from './store';
 
 Vue.config.productionTip = false;
 
 // Setup axios
-axios.defaults.baseURL = 'https://localhost:5001/api';
+axios.defaults.baseURL = 'http://localhost:5000/api';
 Vue.prototype.$http = axios;
 
 // Install Vue extensions
 Vue.use(BootstrapVue);
-Vue.use(PostHub);
+//Vue.use(PostHub);
 
 new Vue({
   store,
