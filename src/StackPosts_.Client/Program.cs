891e72143fecae3535644d660fa7f6d7e3f2b3ad
John Craig <jmcraig32884@gmail.com>
2020-08-25 11:32:00 -0400

    Added services for interfaces and concrete implementations

--- src/StackPosts_.Client/Program.cs
+++ src/StackPosts_.Client/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using StackPosts_.Client.Contracts;
+using StackPosts_.Client.Services;
 
 namespace StackPosts_.Client
 {
@@ -18,6 +20,10 @@ namespace StackPosts_.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddTransient(typeof(IRepositoryService<>), typeof(RepositoryService<>));
+            builder.Services.AddTransient<IPostService, PostService>();
+            builder.Services.AddOptions();
 
             await builder.Build().RunAsync();
         }
