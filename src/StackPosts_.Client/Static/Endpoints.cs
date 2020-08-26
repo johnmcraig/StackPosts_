19b1774f2fc287d6e03f88ec877216a61945d17c
John Craig <jmcraig32884@gmail.com>
2020-08-26 08:58:49 -0400

    Update Endpoints.cs
    Added trailing backslash for post endpoint string.

--- src/StackPosts_.Client/Static/Endpoints.cs
+++ src/StackPosts_.Client/Static/Endpoints.cs
@@ -5,6 +5,6 @@ namespace StackPosts_.Client.Static
     public class Endpoints
     {
         private const string BaseUrl = "https://localhost:5001";
-        public static readonly string PostsEndpoint = $"{BaseUrl}/api/posts";
+        public static readonly string PostsEndpoint = $"{BaseUrl}/api/posts/";
     }
 }
