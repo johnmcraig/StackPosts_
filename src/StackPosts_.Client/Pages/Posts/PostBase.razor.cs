d74b22731a5a64cdd12dcd84026fc37e423aacc0
John Craig <jmcraig32884@gmail.com>
2020-08-30 22:13:27 -0400

    Created PostBase component to handle inherited operations

---
+++ src/StackPosts_.Client/Pages/Posts/PostBase.razor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using StackPosts_.Client.Models;
+using StackPosts_.Client.Services;
+using StackPosts_.Client.Static;
+
+namespace StackPosts_.Client.Pages.Posts
+{
+    public class PostBase : ComponentBase
+    {
+        [Inject] private HttpClient Http { get; set; }
+        // [Inject] private PostService PostService { get; set; }
+        
+        [Parameter] 
+        public int Id { get; set; }
+        
+        [Parameter]
+        public PostModel PostScore { get; set; }
+
+        protected IList<PostModel> PostModelList { get; set; }
+        
+
+        protected override async Task OnInitializedAsync()
+        {
+            // PostModelList = await PostService.GetAll(Endpoints.PostsEndpoint);
+            // PostModelDetails = await PostService.GetSingle(Endpoints.PostsEndpoint, Id);
+            PostModelList = await Http.GetFromJsonAsync<IList<PostModel>>("api/posts");
+            // PostModelDetails = await Http.GetFromJsonAsync<PostModel>($"api/posts/{Id}");
+        }
+
+        protected void UpVote()
+        {
+            PostScore.Score++;
+        }
+
+        protected void DownVote()
+        {
+            PostScore.Score--;
+        }
+    }
+}
