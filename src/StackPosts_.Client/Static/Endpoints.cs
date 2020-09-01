21702c287e99189ca71f834b51f4d53ba4398c37
John Craig <jmcraig32884@gmail.com>
2020-08-31 23:44:00 -0400

    changed endpoints class to static method and cleaned up unused statements

--- src/StackPosts_.Client/Static/Endpoints.cs
+++ src/StackPosts_.Client/Static/Endpoints.cs
@@ -1,8 +1,6 @@
-﻿using System.Buffers.Text;
-
-namespace StackPosts_.Client.Static
+﻿namespace StackPosts_.Client.Static
 {
-    public class Endpoints
+    public static class Endpoints
     {
         private const string BaseUrl = "https://localhost:5001";
         public static readonly string PostsEndpoint = $"{BaseUrl}/api/posts/";
