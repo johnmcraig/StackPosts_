147a96d4b40ebf6948d38e318e294548174dcda1
John Craig <jmcraig32884@gmail.com>
2020-08-31 23:35:59 -0400

    Refactored base componenet to use nav manager, use Interface of post service

--- src/StackPosts_.Client/Pages/Posts/PostBase.razor.cs
+++ src/StackPosts_.Client/Pages/Posts/PostBase.razor.cs
@@ -1,44 +1,44 @@
 ﻿using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using StackPosts_.Client.Contracts;
 using StackPosts_.Client.Models;
-using StackPosts_.Client.Services;
 using StackPosts_.Client.Static;
 
 namespace StackPosts_.Client.Pages.Posts
 {
     public class PostBase : ComponentBase
-    {
-        [Inject] private HttpClient Http { get; set; }
-        // [Inject] private PostService PostService { get; set; }
+    { 
+        [Inject] private IPostService PostService { get; set; }
+        [Inject] private NavigationManager NavManager { get; set; }
         
         [Parameter] 
         public int Id { get; set; }
         
+        protected PostModel PostModel = new PostModel();
+
         [Parameter]
-        public PostModel PostScore { get; set; }
-
-        protected IList<PostModel> PostModelList { get; set; }
+        public IList<PostModel> PostModelList { get; set; }
         
-
         protected override async Task OnInitializedAsync()
         {
-            // PostModelList = await PostService.GetAll(Endpoints.PostsEndpoint);
-            // PostModelDetails = await PostService.GetSingle(Endpoints.PostsEndpoint, Id);
-            PostModelList = await Http.GetFromJsonAsync<IList<PostModel>>("api/posts");
-            // PostModelDetails = await Http.GetFromJsonAsync<PostModel>($"api/posts/{Id}");
+             PostModelList = await PostService.GetAll(Endpoints.PostsEndpoint);
+             PostModel = await PostService.GetSingle(Endpoints.PostsEndpoint, Id);
+        }
+
+        protected void BackToList()
+        {
+            NavManager.NavigateTo("/posts/");
         }
 
         protected void UpVote()
         {
-            PostScore.Score++;
+            PostModel.Score++;
         }
 
         protected void DownVote()
         {
-            PostScore.Score--;
+            PostModel.Score--;
         }
     }
 }
