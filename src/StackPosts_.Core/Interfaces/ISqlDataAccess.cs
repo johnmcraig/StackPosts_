3b68a1526b06fa24e0bd6e1055bacd0c20dd338a
John Craig <jmcraig32884@gmail.com>
2020-11-21 23:45:07 -0500

    Created new interface for Dapper and SQL queries

---
+++ src/StackPosts_.Core/Interfaces/ISqlDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackPosts_.Core.Interfaces
+{
+    public interface ISqlDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionStringName);
+        Task SaveData<T>(string sql, T parameters, string connectionStringName);
+    }
+}
