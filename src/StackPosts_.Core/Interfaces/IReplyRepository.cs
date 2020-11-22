579f4b7f324536ec092ce018b95d199145a15917
John Craig <jmcraig32884@gmail.com>
2020-11-22 17:44:24 -0500

    Removed empty bridge interface as it was unused

--- src/StackPosts_.Core/Interfaces/IReplyRepository.cs
+++
@@ -1,9 +0,0 @@
-using StackPosts_.Core.Entities;
-
-namespace StackPosts_.Core.Interfaces
-{
-    public interface IReplyRepository : IGenericRepository<Reply>
-    {
-        /* An empty bridge */
-    }
-}
