a78e3f7282760572dedfb6e7993444719709aa91
John Craig <jmcraig32884@gmail.com>
2020-11-22 18:06:02 -0500

    Refactored interface to have two mthods that add a reply and check the state of a post existing

--- src/StackPosts_.Core/Interfaces/IPostRepository.cs
+++ src/StackPosts_.Core/Interfaces/IPostRepository.cs
@@ -5,13 +5,9 @@ using StackPosts_.Core.Entities;
 
 namespace StackPosts_.Core.Interfaces
 {
-    public interface IPostRepository : IGenericRepository<Post>
-    {   
-        // Task<IEnumerable<Post>> GetPostsAsync();
-        // Task<Post> GetPostByIdAsync(int id);
-        // Task<Post[]> GetPostByTitle(string title, bool includeReplies = false);
-        // Task<Post[]> GetByDatePosted(DateTime date);
-        // Task<Post> DownVote(int id);
-        // Task<Post> UpVote(int id);
+    public interface IPostRepository : IGenericRepository<Post>, IEfCoreFeatures
+    {
+        void AddReply(Reply entity);
+        Task<bool> PostExists(int id);
     }
 }
