b99fd2525ba59b41a8f74c1bdd6a181c6ec4327e
John Craig <jmcraig32884@gmail.com>
2020-11-23 00:27:56 -0500

    Added generic update method

--- src/StackPosts_.Infrastructure/Data/GenericRepository.cs
+++ src/StackPosts_.Infrastructure/Data/GenericRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using StackPosts_.Core.Entities;
@@ -7,8 +8,8 @@ using StackPosts_.Core.Specifications;
 
 namespace StackPosts_.Infrastructure.Data
 {
-  public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
-  {
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
         private readonly ApplicationDbContext _dbContext;
 
         public GenericRepository(ApplicationDbContext dbContext)
@@ -16,14 +17,16 @@ namespace StackPosts_.Infrastructure.Data
             _dbContext = dbContext;
         }
 
-        public void Add(T entity)
+        public async Task Add(T entity)
         {
-            _dbContext.Add(entity);
+            await _dbContext.AddAsync(entity);
         }
 
-        public void Delete(T entity)
+        public async Task Delete(int id)
         {
-            _dbContext.Remove(entity);
+            var entity = _dbContext.Set<T>().SingleOrDefault(x => x.Id == id);
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
         }
 
         public async Task<T> GetByIdAsync(int id)
@@ -36,9 +39,15 @@ namespace StackPosts_.Infrastructure.Data
             return await _dbContext.Set<T>().ToListAsync();
         }
 
+        public async Task Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
         public async Task<bool> Save()
         {
             return (await _dbContext.SaveChangesAsync()) > 0;
         }
-  }
+    }
 }
